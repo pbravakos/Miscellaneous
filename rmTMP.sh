be087ec4617cca7d846189aab10c2cf575160ce7
pbravakos <pbravakos@biol.uoa.gr>
2020-06-19 14:18:33 +0300

    remove temporary dirs

--- rmTMP.sh
+++ rmTMP.sh
@@ -10,16 +10,28 @@
 # Initial parameters
 EmptyTmp=EmptyUserTmp34.sh
 AvailNodes=PartNode78.txt
-SbatchOut=Deleteme82.txt
 
-# Remove files upon exit.
-trap "rm -f $EmptyTmp $AvailNodes $SbatchOut" EXIT
+
+# Parameters for sbatch
+Output=Deleteme82.txt
+Nnodes=1
+Ntasks=1
+MemPerCpu=100
+
+# Check for the existence of the produced files. If they already exist, exit the script. 
+[[ -f ${Output} ]] && echo "${Output} exists in `pwd`! Please delete, rename or move the file to proceed." >&2 && exit 1
+[[ -f ${AvailNodes} ]] && echo "${AvailNodes} exists in `pwd`! Please delete, rename or move the file to proceed." >&2 && exit 1
+[[ -f ${Output} ]] && echo "${Output} exists in `pwd`! Please delete, rename or move the file to proceed." >&2 && exit 1
+
+
+# Remove produced files upon exit.
+trap "rm -f $EmptyTmp $AvailNodes $Output" EXIT
 
 # We create a regex with all the nodes currently in use by the user. 
 # We want to prevent deleting the temp directories in these nodes, because user is currently running a job on them!
 UserNode=$(squeue | awk -v user="$USER" 'BEGIN {ORS = "|"} $4==user {print $8}' | sed 's/-/\\-/g;s/|$//')
 
-# Check if user is currently running a job or not and export the available nodes.
+# Check if user is currently running a job on a node or not and export the available nodes.
 if [[ -z ${UserNode} ]] 
 then 
     sinfo --Node | awk '$4 ~ /mix|idle/ {print $1, $3}' | sed 's/*$//g' > $AvailNodes
@@ -34,10 +46,10 @@ cat > ${EmptyTmp} <<EOF
 #!/bin/bash
 #SBATCH --partition=${partition}
 #SBATCH --nodelist=${node}
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --mem-per-cpu=100
-#SBATCH --output=${SbatchOut}
+#SBATCH --nodes=${Nnodes}
+#SBATCH --ntasks=${Ntasks}
+#SBATCH --mem-per-cpu=${MemPerCpu}
+#SBATCH --output=${Output}
 EOF
 
 cat >> ${EmptyTmp} <<"EOF"
